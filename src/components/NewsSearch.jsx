c4308760683f78b1aa0d9f442f2f0fa714f83dc2
felixoppongwillborg <fwlixw@gmail.com>
2020-08-12 12:07:49 +0200

    removed last commit and took away key

--- src/components/NewsSearch.jsx
+++ src/components/NewsSearch.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Button, Input } from "semantic-ui-react";
+import axios from "axios";
 
-const NewsSearch = () => {
-  return (
-    <>
-      <Input
-        type="text"
-        data-cy="news-search"
-        placeholder="Input search term" />
-      <Button
-        data-cy="search-submit">
-        Search
+class NewsSearch extends Component {
+  state = {
+    search: "",
+    items: [],
+  };  
+  async performSearch() {
+    debugger
+    const results = await axios.get(`http://newsapi.org/v2/everything?q=${this.state.search}&from=2020-07-12&sortBy=publishedAt&apiKey=<API_KEY>`)
+    this.setState({items: results.data.articles});
+  }
+setValue = (e) => {
+    this.setState({ search: e.target.value });
+  };
+  render() {
+    let displayNews = this.state.items.map(items => {
+      return (
+      <li>{items.articles}</li>
+      )
+    })
+    return (
+      <>
+        <Input
+          onChange={(e) => this.setValue(e)}
+          type="text"
+          data-cy="news-search"
+          placeholder="Input search term"
+        />
+        <Button onClick={this.performSearch.bind(this)} data-cy="search-submit">
+          Search
         </Button>
-    </>
-  )
+        <ul>{displayNews}</ul>
+      </>
+    );
+  }
 }
-
-export default NewsSearch
+export default NewsSearch;
